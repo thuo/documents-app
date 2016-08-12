13ec831e0df75a10d37fdc63192ee00d7ea5a1d0
Humphrey Thuo <humphrey.thuo@andela.com>
2016-08-12 12:36:57 +0300

    [Feature #128053725] Implement editing of user profile

--- client/reducers/users.js
+++ client/reducers/users.js
@@ -1,5 +1,6 @@
 import {
   USER_GET_REQUEST, USER_GET_SUCCESS, USER_GET_FAILURE,
+  USER_UPDATE_REQUEST, USER_UPDATE_SUCCESS, USER_UPDATE_FAILURE,
 } from 'app/actions/ActionTypes';
 
 export function userPage(state = { user: null, error: null,
@@ -20,6 +21,28 @@ export function userPage(state = { user: null, error: null,
     case USER_GET_FAILURE:
       return {
         user: null,
+        error: action.error,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+}
+
+export function editProfile(state = { error: null, loading: false }, action) {
+  switch (action.type) {
+    case USER_UPDATE_REQUEST:
+      return {
+        error: null,
+        loading: true,
+      };
+    case USER_UPDATE_SUCCESS:
+      return {
+        error: null,
+        loading: false,
+      };
+    case USER_UPDATE_FAILURE:
+      return {
         error: action.error,
         loading: false,
       };
