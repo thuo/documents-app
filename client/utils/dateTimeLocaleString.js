d4dc310e2fd937aa97d7ca4e9e962ade9dec3839
Humphrey Thuo <humphrey.thuo@andela.com>
2016-08-20 18:50:08 +0300

    [Chore #128842049] Remove moment to reduce bundle size

--- client/utils/dateTimeLocaleString.js
+++ client/utils/dateTimeLocaleString.js
@@ -1,3 +1,35 @@
-import moment from 'moment';
+const months = [
+  'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+  'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
+];
 
-export default date => moment(date).format('Do MMM YYYY, h:mm a');
+function getOrdinal(number) {
+  if (number > 10 && number < 20) {
+    return 'th';
+  }
+  switch (number % 10) {
+    case 1:
+      return 'st';
+    case 2:
+      return 'nd';
+    case 3:
+      return 'rd';
+    default:
+      return 'th';
+  }
+}
+
+export default date => {
+  const d = new Date(date);
+  const day = d.getDate();
+  const ordinal = getOrdinal(day);
+  const month = months[d.getMonth()];
+  const year = d.getFullYear();
+  const hours = d.getHours();
+  let minutes = d.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  const period = hours < 12 ? 'am' : 'pm';
+  return `${day}${ordinal} ${month} ${year}, ${hours % 12}:${minutes}${period}`;
+};
